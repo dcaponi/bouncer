0334f1aef9e028e42695b2be07f983faf54cb1f3
Dominick Caponi <d.caponi1@gmail.com>
2019-07-27 11:12:45 -0700

    adds health check middleware

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
+require "./lib/middleware/health.rb"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -21,7 +22,7 @@ module Bouncer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.middleware.use Middleware::Health
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
