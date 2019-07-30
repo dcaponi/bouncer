a8ea232c2140a8eed1fb47f244afa62f92281f49
Dominick Caponi <d.caponi1@gmail.com>
2019-07-29 20:04:28 -0700

    fixes cookie storage issue

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,20 +1,19 @@
 class TokensController < ApplicationController
-
   def access_token
     @user = User.find_by_email(user_params['email'])
 
     if @user.authenticate(user_params['password'])
       token = JsonWebToken.encode({"id": @user.id})
-      time = Time.now + 24.hours.to_i
-      render json: {token: token, exp: time.strftime("%m-%d-%Y %H:%M"), username: @user.email}, status: :ok
+      cookies[:login] = {
+        :value => token,
+        :expires => 1.hour.from_now,
+        httponly: true
+      }
+      render json: {username: @user.email}, status: :ok
     else
       render json: {'unauthorized': 'incorrect login credentials'}, status: :unauthorized
     end
 
-  end
-
-  def test
-    binding.pry
   end
 
   private
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost', 'developerdom.com'
+    origins 'localhost', 'bouncer.developerdom.com'
 
     resource '*',
+      credentials: true,
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
