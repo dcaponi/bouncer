c97f76ece5704c47012d1a0dcf66a0e82ff8a1b1
Dominick Caponi <d.caponi1@gmail.com>
2019-08-11 10:50:43 -0700

    adds policies controller and removes unnecesary methods/comments

--- app/controllers/policies_controller.rb
+++ app/controllers/policies_controller.rb
@@ -1,13 +1,31 @@
 class PoliciesController < ApplicationController
+  before_action :set_policy, only: [:destroy]
   def create
+    new_policy_requests = []
+    writing_errors = []
     user_id = JsonWebToken.decode(cookies[:login])["id"]
-    resource_server = ResourceServer.find_by_name(policy_params[:resource_server])
-    resource = Resource.find_by_name(policy_params[:resource])
-    policy = Policy.new(user_id: user_id, resource_server_id: resource_server.id, resource_id: resource.id, permission: policy_params[:permission])
-    if policy.save
-      render json: {policy: {id: policy.id, resource_server: resource_server.name, resource: resource.name}}
+    if user_id
+      policy_params[:policies].each do |param|
+        resource_server = ResourceServer.find_by_name(param[:resource_server])
+        resource = Resource.find_by_name(param[:resource])
+        new_policy_requests << {user_id: user_id, resource_server_id: resource_server.id, resource_id: resource.id, permission: param[:permission]}
+      end
+
+      new_policies = Policy.create(new_policy_requests)
+
+      new_policies.each_with_index do |p, i|
+        writing_errors << { index: i, message: p.errors.full_messages} if p.errors.any?
+      end
+
+      new_policies.each { |p| p.destroy } if writing_errors.any?
+
+      unless writing_errors.any?
+        render json: {policies: new_policies}
+      else
+        render json: {errors: writing_errors}, status: :unprocessable_entity
+      end
     else
-      render json: policy.errors, status: :unprocessable_entity
+      render json: {'unauthorized': 'not authorized to perform this action'}, status: :unauthorized
     end
   end
 
@@ -17,14 +35,30 @@ class PoliciesController < ApplicationController
   end
 
   def update
-
+    # TODO holy fuck this method is ass... fix!!!
+    user_id = JsonWebToken.decode(cookies[:login])["id"]
+    ids = policy_params[:policies].map { |pp| pp[:id] }
+    policies_to_change = Policy.where(user_id: user_id, id: ids)
+    policies_to_change.map do |p|
+      p.update(permission: policy_params[:policies].select { |i| i[:id] == "56a4c6d3-240c-4024-a8ab-d9f2bd85f6bf" }.first[:permission])
+    end
   end
 
   def destroy
+    if @policy
+      @policy.destroy
+    else
+      render json: {'bad request': 'the policy with the given params was not found'}, status: 401
+    end
+  end
 
+  private;
+  def set_policy
+    user_id = JsonWebToken.decode(cookies[:login])["id"]
+    @policy = Policy.where(user_id: user_id, id: params[:id]).first
   end
 
   def policy_params
-    params.require(:policy).permit(:resource_server, :resource, :permission)
+    params.permit({policies: [:id, :resource_server, :resource, :permission]})
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,14 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: [:show, :update]
 
-  # GET /users
-  # GET /users.json
   def index
     # TODO this will be needed if/when the notion of organizations is introduced
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
-    user_id = JsonWebToken.decode(cookies[:login])["id"]
-    @user = User.find(user_id)
     render json: {user: {id: @user.id, name_first: @user.name_first, name_last: @user.name_last, email: @user.email, created_at: @user.created_at}}, status: :created
   end
 
-  # POST /users
-  # POST /users.json
   def create
     redirect_url = params[:user].delete(:redirect_url) || '/'
     @user = User.find_by_email(user_params[:email]) || User.new(user_params)
@@ -38,8 +30,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     if @user
       if email_confirm_params[:email_confirm_token]
@@ -57,24 +47,17 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
-  # TODO get rid of this or lock down
-  def destroy
-    @user.destroy
-  end
+  private;
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       if email_confirm_params[:email_confirm_token]
         @user = User.find_by_email_confirm_token(email_confirm_params[:email_confirm_token])
       else
-        @user = User.find(params[:id])
+        user_id = JsonWebToken.decode(cookies[:login])["id"]
+        @user = User.find(user_id)
       end
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def email_confirm_params
       params.permit(:redirect_url, :email_confirm_token)
     end
