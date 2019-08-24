52f5db4dfa7d461198cff63589a79cfcd43c0bc8
Dominick Caponi <d.caponi1@gmail.com>
2019-08-23 21:20:33 -0700

    environment-based domains

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -6,8 +6,14 @@ class TokensController < ApplicationController
       if @user.is_validated_user?
         if @user.authenticate(user_params['password'])
           token = JsonWebToken.encode({"id": @user.id})
-          cookies[:login] = { :value => token, :expires => 1.hour.from_now, :httponly => true, :domain => '.developerdom.com' }
-          render json: {username: @user.email}, status: :ok
+          domain = ENV['COOKIE_DOMAIN']
+          cookies[:login] = {
+            :value => token,
+            :expires => 1.hour.from_now,
+            :httponly => true,
+            :domain => domain
+          }
+          render json: { username: @user.email }, status: :ok
         else
           render json: {'unauthorized': 'incorrect login credentials'}, status: :unauthorized
         end
